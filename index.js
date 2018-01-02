ad524f98a90d119cc59ffa296e56d9df4ccc3024
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-01-02 14:02:30 +0100

    Add some commands

--- index.js
+++ index.js
@@ -3,26 +3,44 @@ const request = require("request");
 
 const bot = new TeleBot({
     token: process.env.TOKEN, // Required. Telegram Bot API token.
-    polling: { // Optional. Use polling.
-        interval: 1000, // Optional. How often check updates (in ms).
-        timeout: 0, // Optional. Update polling timeout (0 - short polling).
-        limit: 100, // Optional. Limits the number of updates to be retrieved.
-        retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
-    },
-    allowedUpdates: [], // Optional. List the types of updates you want your bot to receive. Specify an empty list to receive all updates.
-    usePlugins: [], // Optional. Use user plugins from pluginFolder.
+});
+
+// TODO List available commands
+commands = [
+    { command: "/start", desc: "Start a conversation with the Bot" },
+    { command: "/welcome", desc: "Start a conversation with the Bot" },
+    { command: "/help", desc: "Print this message" },
+    { command: "/say {message}", desc: "Say the message passed in parameters" },
+    { command: "/joke", desc: "Tell a joke" },
+]
+
+bot.on("*", (msg) => {
+    // Log every message
+    //console.log(msg)
 });
 
 bot.on(["/start", "/welcome"], (msg) => msg.reply.text("Hello ! I'm a simple bot."));
+
+bot.on("/help", (msg) => {
+    var reply = "";
+    commands.forEach(function (command) {
+        reply += command.command + " : " + command.desc + "\n";
+    });
+    msg.reply.text(reply);
+});
+
+bot.on(/^\/say (.+)$/, (msg, props) => {
+    const text = props.match[1];
+    return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
+});
 
 bot.on("/joke", (msg) => {
     url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
 
     request({
         url: url,
-        method: "GET",
-
-    }, function (error, response, body ) {
+        method: "GET"
+    }, function (error, response, body) {
         if (error) throw error;
         var joke = JSON.parse(body);
         msg.reply.text(joke.setup + "\n" + joke.punchline);
