424e1654a67cd839384d099005adb959c0277e95
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-02-01 14:56:28 +0100

    Bot is dumb again

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ var bot = new TeleBot({ token: process.env.TOKEN });
 
 // USER
 var joke = (msg) => {
-    if (!canITalk(msg)) {
-        return;
-    }
-
     url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
 
     request({
