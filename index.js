97b71c620c3eefd8ef668317d16376f0f142edd7
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-02-02 14:49:42 +0100

    Try to force heroku to be awake

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const options = {
     polling: true
 };
 
+if (process.env.HEROKU_APP_NAME != undefined) {
+    var http = require("http");
+    setInterval(function () {
+        http.get(`http://${process.env.HEROKU_APP_NAME}.herokuapp.com`);
+    }, 300000); // every 5 minutes (300000)
+}
+
 var telebot = new TelegramBot(token, options);
 
 var bot = new Bot(telebot);
