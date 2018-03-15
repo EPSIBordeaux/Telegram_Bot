a3f3881b47ee9afa8e1b089033925aca0d750760
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-03-15 11:21:24 +0100

    Add AirBrake bug tracking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const MyChatBot = require('./bot')
+const AirbrakeClient = require('airbrake-js')
 
 const token = process.env.TOKEN
 const options = {
@@ -9,3 +10,15 @@ const options = {
 
 const bot = new MyChatBot(token, options)
 console.log(bot.options)
+
+if (process.env.CIRCLECI === undefined) {
+  var airbrake = new AirbrakeClient({
+    projectId: process.env.PROJECT_ID,
+    projectKey: process.env.PROJECT_KEY
+  })
+
+  airbrake.addFilter(function (notice) {
+    notice.context.environment = process.env.NODE_ENV
+    return notice
+  })
+}
